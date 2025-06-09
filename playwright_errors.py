570afab3755f726db94e8cd4fc11974de9e58189
Nikita <110235307+nberzerkov@users.noreply.github.com>
2025-06-09 22:01:42 +0500

    5.5 Углубление в Playwright

--- playwright_errors.py
+++ playwright_errors.py
@@ -6,7 +6,18 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
 
-    page.goto(login_page)
+    page.goto(login_page, wait_until='networkidle')
 
-    h6 = page.get_by_test_id('authentication-ui-course-title-text')
-    expect(h6).to_be_visible()
+    # h6 = page.get_by_test_id('authentication-ui-course-title-text')
+    # expect(h6).to_be_visible()
+
+    # login_btn = page.get_by_test_id('login-page-login-button')
+    # login_btn.fill('unknown')
+
+    # page.evaluate(
+    #     """
+    #         const title = document.getElementById('authentication-ui-course-title-text');
+    #         title.textContent = 'Hello';
+    #     """
+    # )
+
