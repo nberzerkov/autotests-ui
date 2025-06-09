da74b5fe8cb5a65719c54acc7bb40bae6c334910
Nikita <110235307+nberzerkov@users.noreply.github.com>
2025-06-09 21:44:00 +0500

    5.5 Углубление в Playwright. Работа с ошибками при написании автотестов. 1. Элемент/локатор не найден на странице

---
+++ playwright_errors.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright, expect
+
+login_page = 'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login'
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto(login_page)
+
+    h6 = page.get_by_test_id('authentication-ui-course-title-text')
+    expect(h6).to_be_visible()
