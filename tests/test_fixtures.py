0688613e108543592cbdb09c32391c5992f3836d
Nikita <110235307+nberzerkov@users.noreply.github.com>
2025-06-19 00:51:53 +0500

    6.5 Фикстуры Pytest. Практикуемся в написании простых фикстур

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -25,11 +25,11 @@ def browser():
 
 class TestUserFlow:
     def test_user_can_login(self, settings, user, browser):
-        ...
+        pass
 
     def test_user_can_create_course(self, settings, user, browser):
-        ...
+        pass
 
 class TestAccountFlow:
-    def test_user_account(self, settings, browser):
-        ...
+    def test_user_account(self, settings, user, browser):
+        pass
