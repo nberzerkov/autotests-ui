185b0b129ffe8160d9ad41f12ace6b74bed000bc
Nikita <110235307+nberzerkov@users.noreply.github.com>
2025-10-12 11:51:39 +0500

    9.1 Настройки автотестов с Pydantic

---
+++ pydantic_user.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    is_active: bool = True
+
+
+# user_data = {
+#     "id": 1,
+#     "username": "nick",
+#     "email": "nickmello@mail.ru"
+# }
+#
+#
+# user = User(**user_data)
+# print(user)
+
+
+invalid_user_data = {
+    "id": 'asd',
+    "username": "nick",
+    "email": "nickmello@mail.ru"
+}
+
+invalid_user = User(**invalid_user_data)
+print(invalid_user)
