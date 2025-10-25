de2b1cf3004d0e1a56f5265490b2fd6bd044104a
Nikita <110235307+nberzerkov@users.noreply.github.com>
2025-10-25 14:45:56 +0500

    9.6 Добавляем информацию о переменных окружения. Практика работы с переменными окружения в Allure

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,8 +1,20 @@
+import sys
+import platform
+from pathlib import Path
+from dotenv import dotenv_values
+
 from config import settings
 
 def create_allure_environment_file():
-    items = [f'{key}={value}' for key, value in settings.model_dump().items()]
-    properties = '\n'.join(items)
+    setting_items = [f'{key}={value}' for key, value in settings.model_dump().items()]
+    os_info = f'os_info={platform.system()}, {platform.release()}'
+
+    python_ver_str = sys.version.replace("\n", " ")
+    python_version = f"python_version={python_ver_str}"
+
+    all_items = setting_items + [os_info, python_version]
+
+    properties = '\n'.join(all_items)
 
     with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
         file.write(properties)
